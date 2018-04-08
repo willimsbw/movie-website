52890a913f991efea466ebb0ed8565307860cfef
willimsbw <willimsbw@gmail.com>
2018-04-08 19:39:54 -0400

    Aligned with pep8 standards

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 class Video():
     """
     This class lets you construct Video objects containing the video's:
@@ -11,6 +12,7 @@ class Video():
     def __init__(self, video_title, video_duration):
         self.title = video_title
         self.duration = video_duration
+
 
 class Movie(Video):
     """
@@ -25,7 +27,8 @@ class Movie(Video):
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, plot_summary, movie_trailer, poster_image, movie_duration):
+    def __init__(self, movie_title, plot_summary, movie_trailer, poster_image,
+                 movie_duration):
         Video.__init__(self, movie_title, movie_duration)
         self.trailer_youtube_url = movie_trailer
         self.poster_image_url = poster_image
@@ -33,6 +36,7 @@ class Movie(Video):
 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
+
 
 class TvShow(Video):
     """
@@ -46,7 +50,8 @@ class TvShow(Video):
     duration: int. number of minutes long the episode is
     """
 
-    def __init__(self, show_title, episode_season, episode_number, show_station, episode_duration):
+    def __init__(self, show_title, episode_season, episode_number,
+                 show_station, episode_duration):
         Video.__init__(self, show_title, episode_duration)
         self.season = episode_season
         self.episode = episode_number
