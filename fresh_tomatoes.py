8f6d87e030cdad8d2eb2dba7477de2888743edc9
willimsbw <willimsbw@gmail.com>
2018-04-09 21:57:38 -0400

    Added overview

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -163,9 +163,9 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie["title"],
+            movie_summary=movie["overview"],
             poster_image_url=poster_url,
             trailer_youtube_id=trailer_id
-            movie_summary=movie["overview"]
         )
     return content
 
