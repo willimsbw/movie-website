45f1dbd3d1a38648a1dd6758cbf9847663011669
Bryan Williams <Bryan.williams@achiever.com>
2018-04-10 15:07:29 -0400

    Removed movie title

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -141,9 +141,6 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}">
     <div class="thumbnail" data-toggle="modal" data-target="#trailer">
         <img src="{poster_image_url}">
-        <div class="caption">
-            <h2>{movie_title}</h2>
-        </div>
     </div>
 </div>
 '''
@@ -172,7 +169,6 @@ def create_movie_tiles_content(movies):
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
-            movie_title=movie["title"],
             poster_image_url=poster_url,
             trailer_youtube_id=trailer_id
         )
