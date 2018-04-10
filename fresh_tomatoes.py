ca00427dc3d76fa4e322dd8645266fef6bc3984f
Bryan Williams <bryanwc@Bryans-MacBook-Pro.local>
2018-04-09 20:34:02 -0400

    Added TMBD logo to navbar

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -107,7 +107,9 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">
+                <img src="https://www.themoviedb.org/static_cache/v4/logos/powered-by-rectangle-green-dcada16968ed648d5eb3b36bbcfdd8cdf804f723dcca775c8f2bf4cea025aad6.svg" width="60" height="30" alt="">
+            </a>
           </div>
         </div>
       </div>
